29f50ed75398077a965ccb4386a55893adfd8b4c
Gitudon <oniku.udon.029@gmail.com>
2026-04-15 10:15:37 +0900

    Sin_Meikai_C_Nyumon: 13-9まで

---
+++ Sin_Meikai_C_Nyumon/13/13-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *fp;
+    double pi = 3.14159265358979323846;
+    printf("変数 pi から取り出した円周率は%23.21fです。\n", pi);
+    if ((fp = fopen("PI.bin", "w")) == NULL)
+        printf("\aファイルをオープンできません。\n");
+    else
+    {
+        fwrite(&pi, sizeof(double), 1, fp);
+        fclose(fp);
+    }
+    if ((fp = fopen("PI.bin", "r")) == NULL)
+        printf("\aファイルをオープンできません。\n");
+    else
+    {
+        fread(&pi, sizeof(double), 1, fp);
+        printf("ファイルから読み取った円周率は%23.21fです。\n", pi);
+        fclose(fp);
+    }
+    return 0;
+}
